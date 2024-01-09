99c4a33c5727db708b594f61544125fd2dfacd1d
Hooorus <2479711422@qq.com>
2024-01-09 16:24:01 +0800

    finish sprain web data sync function

--- Redis2Mysql.py
+++ Redis2Mysql.py
@@ -4,70 +4,120 @@ import pymysql
 import redis
 
 
-def sync_redis_2_mysql(mysql_host: str, mysql_port: int, mysql_user: str, mysql_password: str, mysql_database: str,
-                       redis_host: str, redis_port: int, redis_db: int):
-    try:
-        # 连接到 MySQL 数据库，反正xywy, haodf, sprain都是在一个数据库里
-        mysql_connection = pymysql.connect(host=mysql_host, port=mysql_port, user=mysql_user, password=mysql_password,
-                                           database=mysql_database)
-        logging.info(f"\033[32m====mysql_results====\n{mysql_connection}\033[0m")
-        mysql_cursor = mysql_connection.cursor()
-        logging.debug(f"\033[34m====mysql_cursor====\n{mysql_cursor}\033[0m")
-        # mysql_cursor.execute("SELECT * FROM scrapy_sprain_doc")
-        # mysql_results = mysql_cursor.fetchall()
-        # logging.info(f"\033[32m====mysql_results====\n{mysql_results}\033[0m")
-        # for row in mysql_results:
-        #     logging.info(f"\033[32m====QUERY MYSQL====\n{row}\033[0m")
+class Redis2Mysql:
+    def __init__(self, mysql_host: str, mysql_port: int, mysql_user: str, mysql_password: str, mysql_database: str,
+                 redis_host: str, redis_port: int, redis_db: int):
+        # ------------初始化Redis------------
+        self.redis_connection = redis.StrictRedis(host=redis_host, port=redis_port, db=redis_db)
+        logging.info(f"\033[32m====self.redis_connection====\n{self.redis_connection}\033[0m")
+        # ------------初始化Mysql------------
+        self.mysql_connection = pymysql.connect(host=mysql_host, port=mysql_port, user=mysql_user,
+                                                password=mysql_password,
+                                                database=mysql_database)
+        logging.info(f"\033[32m====self.mysql_connection====\n{self.mysql_connection}\033[0m")
+        self.mysql_cursor = self.mysql_connection.cursor()
+        logging.debug(f"\033[34m====self.mysql_cursor====\n{self.mysql_cursor}\033[0m")
 
-        # 定义数据表名
-        scrapy_xywy = 'scrapy_xywy'
-        scrapy_sprain_doc = 'scrapy_sprain_doc'
-        scrapy_haodf = 'scrapy_haodf'
+    def sync_redis_2_mysql(self):
+        try:
+            # 定义数据表名
+            scrapy_xywy = 'scrapy_xywy'
+            scrapy_sprain_doc = 'scrapy_sprain_doc'
+            scrapy_haodf = 'scrapy_haodf'
+            redis_results = self.redis_connection.keys('*')  # 以示例方式获取所有键
+            # while True:
+            #     redis_results = redis_connection.scan(start_id, count=200)
+            # redis_results = redis_results[:2]
 
-        # 连接到 Redis
-        redis_connection = redis.StrictRedis(host=redis_host, port=redis_port, db=redis_db)
+            for key in redis_results:
+                key = key.decode('utf-8')
+                logging.info(f"\033[32m====QUERY KEY====\n{key}\033[0m")
+                # 获取Redis键的前缀
+                prefix = key.split(':')[0]
+                if prefix == 'xywy':
+                    # 将Redis键同步到MySQL表`xywy`中
+                    self.transfer_xywy(key, scrapy_xywy)
+                elif prefix == 'haodf':
+                    # 将Redis键同步到MySQL表`haodf`中
+                    self.transfer_haodf(key, scrapy_haodf)
+                elif prefix == 'sprain':
+                    # 将Redis键同步到MySQL表`sprain`中
+                    self.transfer_sprain(key, scrapy_sprain_doc)
 
-        # TODO 执行 Redis 查询特定的数据，来进行逐行同步到mysql中
-        redis_results = redis_connection.keys('*')  # 以示例方式获取所有键
-        for key in redis_results:
-            logging.info(f"\033[32m====QUERY KEY====\n{key}\033[0m")
-            # 获取Redis键的前缀
-            prefix = key.split(':')[0]
-            if prefix == 'xywy':
-                # 将Redis键同步到MySQL表`xywy`中
-                transfer_xywy(scrapy_xywy)
-            elif prefix == 'haodf':
-                # 将Redis键同步到MySQL表`haodf`中
-                transfer_xywy(scrapy_sprain_doc)
-            elif prefix == 'sprain':
-                # 将Redis键同步到MySQL表`sprain`中
-                transfer_xywy(scrapy_haodf)
+        except pymysql.Error as mysql_error:
+            logging.error(f"\033[31m====Mysql Error====\n{mysql_error}\033[0m")
 
-    except pymysql.Error as mysql_error:
-        logging.info(f"\033[33m====Mysql Error====\n{mysql_error}\033[0m")
+        except redis.RedisError as redis_error:
+            logging.error(f"\033[31m====Redis Error====\n{redis_error}\033[0m")
 
-    except redis.RedisError as redis_error:
-        logging.info(f"\033[33m====Redis Error====\n{redis_error}\033[0m")
+        finally:
+            # 关闭 MySQL 连接和游标
+            if 'mysql_cursor' in locals() and self.mysql_cursor is not None:
+                self.mysql_cursor.close()
+            if 'mysql_connection' in locals() and self.mysql_connection is not None:
+                self.mysql_connection.close()
 
-    finally:
-        # 关闭 MySQL 连接和游标
-        if 'mysql_cursor' in locals() and mysql_cursor is not None:
-            mysql_cursor.close()
-        if 'mysql_connection' in locals() and mysql_connection is not None:
-            mysql_connection.close()
+            # 关闭 Redis 连接
+            if 'redis_connection' in locals() and self.redis_connection is not None:
+                self.redis_connection.close()
 
-        # 关闭 Redis 连接
-        if 'redis_connection' in locals() and redis_connection is not None:
-            redis_connection.close()
+    # ----------------寻医问药网站的同步器----------------
+    def transfer_xywy(self, key: str, db_name: str):
+        value = self.redis_connection.hgetall(key)
 
+        # --------解码所有键值对--------
+        value = {k.decode('utf-8'): v.decode('utf-8') for k, v in value.items()}
+        logging.info(f"\033[32m====redis value====\n{value}\033[0m")
 
-def transfer_xywy(db_name: str):
-    pass
+        title = self.mysql_connection.escape(value['title'])
+        quest = self.mysql_connection.escape(value['quest'])
+        url = self.mysql_connection.escape(value['url'])
+        answer = self.mysql_connection.escape(value['answer'])
+        doc_name = self.mysql_connection.escape(value['doc_name'])
+        doc_posts = self.mysql_connection.escape(value['doc_posts'])
+        department = self.mysql_connection.escape(value['department'])
+        hos_grade = self.mysql_connection.escape(value['hos_grade'])
+        hos_name = self.mysql_connection.escape(value['hos_name'])
 
+    # ----------------好大夫网站的同步器----------------
+    def transfer_haodf(self, key: str, db_name: str):
+        value = self.redis_connection.hgetall(key)
 
-def transfer_haodf(db_name: str):
-    pass
+    # ----------------春雨医生网站的同步器----------------
+    def transfer_sprain(self, key: str, db_name: str):
+        value = self.redis_connection.hgetall(key)
 
+        # --------解码所有键值对--------
+        value = {k.decode('utf-8'): v.decode('utf-8') for k, v in value.items()}
+        logging.info(f"\033[32m====redis value====\n{value}\033[0m")
+        
+        issue_title = self.mysql_connection.escape(value['issue_title'])
+        issue_desc = self.mysql_connection.escape(value['issue_desc'])
+        case_url = self.mysql_connection.escape(value['case_url'])
+        answer = self.mysql_connection.escape(value['answer'])
+        already_parsed = self.mysql_connection.escape(value['already_parsed'])
 
-def transfer_sprain(db_name: str):
-    pass
+        # -------处理字符串-------这里不在VALUE里面加'的原因是escape已经帮我们加上去了
+        sql = f"""
+            INSERT INTO {db_name} (issue_title, issue_desc, answer, case_url, already_parsed)
+            VALUES ({issue_title}, {issue_desc}, {answer}, {case_url}, {already_parsed})
+            """
+        logging.debug(f"\033[34m====SQL====\n{sql}\033[0m")
+
+        redis_pipe = self.redis_connection.pipeline()  # 获取 Redis 客户端对象
+        redis_pipe.multi()  # 开启事务
+        self.mysql_connection.begin()  # 开启事务
+
+        try:
+            self.mysql_cursor.execute(sql)
+            self.mysql_connection.commit()
+            logging.info("\033[32m====Mysql row committed====\033[0m")
+            redis_pipe.delete(key)
+            redis_result = redis_pipe.execute()
+            logging.info(f"\033[32m====Redis row deleted:{redis_result}====\033[0m")
+        except Exception as e:
+            # 发生异常，回滚事务
+            self.mysql_connection.rollback()
+            # self.redis_pipe.execute()  # 自动回滚
+            logging.error(f"\033[31m====SQL Error Occurred: {e}====\033[0m")
+            raise e
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 import time
 import schedule
 
-from Redis2Mysql import sync_redis_2_mysql
+import Redis2Mysql
 
 # MySQL 连接信息
 mysql_host = '192.168.1.204'
@@ -21,16 +21,25 @@ logging.basicConfig(level=logging.DEBUG)
 
 def schedule_job():
     print("====Executing scheduled job: sync_redis_2_mysql====")
-    sync_redis_2_mysql(mysql_host, mysql_port, mysql_user, mysql_password,
-                       mysql_database, redis_host, redis_port, redis_db)
+    redis2mysql_instance = Redis2Mysql.Redis2Mysql(mysql_host=mysql_host,
+                                                   mysql_port=mysql_port,
+                                                   mysql_user=mysql_user,
+                                                   mysql_password=mysql_password,
+                                                   mysql_database=mysql_database,
+                                                   redis_host=redis_host,
+                                                   redis_port=redis_port,
+                                                   redis_db=redis_db)
+
+    redis2mysql_instance.sync_redis_2_mysql()
     print("====Scheduled job: sync_redis_2_mysql Finished====")
 
 
 # schedule.every().day.at("20:00").do(schedule_job)
 # schedule.every(5).minutes.do(schedule_job)
-schedule.every(10).seconds.do(schedule_job)
+schedule.every(5).seconds.do(schedule_job)
 
 if __name__ == '__main__':
+    print("====Welcome to Database Sync====")
     while True:
         schedule.run_pending()
         time.sleep(5)  # 避免不必要的消耗
